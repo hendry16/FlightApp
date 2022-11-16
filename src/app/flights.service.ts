bf6c6e418637cdc04bff452a688fc52c486897e3
hendry16 <hendrry16@gmail.com>
2022-11-16 10:57:58 +0000

    ts flight app

---
+++ src/app/flights.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {Flight} from './flight.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FlightsService {
+  flights: Flight[] = [
+    {
+      origin: 'Miami',
+      destination: 'Chicago',
+      flightNumber: 345,
+      depart: new Date('2020-02-25T23:18:21932Z'),
+      arrive: new Date('2020-02-25T23:21:21932Z'),
+      nonstop: true
+    },
+    {
+      origin: 'Medan',
+      destination: 'Bandung',
+      flightNumber: 156,
+      depart: new Date('2020-02-25T15:18:21932Z'),
+      arrive: new Date('2020-02-25T1y:21:21932Z'),
+      nonstop: false
+    },
+  ]; 
+  constructor() { }
+  getFlights() {
+    return this.flights;
+  }
+  postFlight(flight: Flight) {
+
+  }
+  deleteFlight(id: number) {
+
+  }
+}
